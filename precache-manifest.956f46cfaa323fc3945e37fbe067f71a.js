ae43d3a20aad28cc4ffa94ce6899a774de673fb2
tonnon <lucastonnon@gmail.com>
2021-06-03 17:32:01 -0300

    Updates

---
+++ precache-manifest.956f46cfaa323fc3945e37fbe067f71a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b56c34c722f89df0f5c3cc62b43c24",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0a4da13d63fea66fe47a",
+    "url": "/portfolio/static/css/main.00749886.chunk.css"
+  },
+  {
+    "revision": "baa514434be8d6abce04",
+    "url": "/portfolio/static/js/2.9a2ccd6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.9a2ccd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4da13d63fea66fe47a",
+    "url": "/portfolio/static/js/main.9d89acd3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0cf19446792ca500335801ddb80813f1",
+    "url": "/portfolio/static/media/alongnight.0cf19446.gif"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/portfolio/static/media/bethehero.1f4b5508.png"
+  },
+  {
+    "revision": "0e979812d02bf180aeb9d232853c000a",
+    "url": "/portfolio/static/media/cv-tonon.0e979812.pdf"
+  },
+  {
+    "revision": "cc8e1779bad119052b250aa89c4db954",
+    "url": "/portfolio/static/media/cybertetromino.cc8e1779.gif"
+  },
+  {
+    "revision": "7649c61b5972bb46474c1fc252949074",
+    "url": "/portfolio/static/media/disguised.7649c61b.gif"
+  },
+  {
+    "revision": "f498078ca31c65745842399f40382e2f",
+    "url": "/portfolio/static/media/fcknpidgey.f498078c.gif"
+  },
+  {
+    "revision": "32af9086b6183b8ed1973e45f57b0d7d",
+    "url": "/portfolio/static/media/haterspace.32af9086.gif"
+  },
+  {
+    "revision": "1d0ff670a00853a2a46c0cd136c22d2c",
+    "url": "/portfolio/static/media/helptoner.1d0ff670.gif"
+  },
+  {
+    "revision": "39560b274f509c8b096dc6e7285aceb0",
+    "url": "/portfolio/static/media/looperman.39560b27.gif"
+  },
+  {
+    "revision": "f3d47d2c6e153f2efaa7cf404c6618d7",
+    "url": "/portfolio/static/media/orbital.f3d47d2c.gif"
+  },
+  {
+    "revision": "7bc79d766c1c175868d0df0728092230",
+    "url": "/portfolio/static/media/rickandmorty.7bc79d76.gif"
+  },
+  {
+    "revision": "dd18b45dd55162304fdd9b11c34064be",
+    "url": "/portfolio/static/media/spacerocker.dd18b45d.gif"
+  }
+]);
