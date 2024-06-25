c8069bfedd1287ea76302a4ee74a1e249d5844ec
tonnon <lucastonnon@gmail.com>
2024-06-25 10:28:15 -0300

    projects

--- app/ui/projects/Projects.tsx
+++ app/ui/projects/Projects.tsx
@@ -5,23 +5,24 @@ import Link from 'next/link';
 const Projects = () => {
   return (
     <div className='absolute top-[150px] z-[2]'>
-        <div className="flex justify-center flex-wrap gap-[71px] hover:cursor-pointer px-[50px]">
-        {projectslist.map((project, id) => (
-            <Link href={project.url} target='__blank' key={id} className="w-[383px] h-full rounded-lg border border-neon shadow-neon">
-                <Image
-                    src={project.img}
-                    alt={project.title}
-                    width={240}
-                    height={320}
-                    className='w-full object-contain rounded-t-lg'
-                />
-                <div className='relative p-4 bg-[#03001417] backdrop-blur-md rounded-b-lg'>
-                    <h2 className='text-1xl font-semibold text-white text-center'>
-                        {project.title}
-                    </h2>
-                </div>
-            </Link>
-        ))}
+        <div className="flex justify-center flex-wrap gap-[85px] hover:cursor-pointer">
+            {projectslist.map((project, id) => (
+                <Link href={project.url} target='__blank' key={id} className="w-[383px] rounded-lg border border-neon shadow-neon">
+                      <div className="relative w-full h-0 pb-[83.54%] rounded-t-lg overflow-hidden">
+                    <Image
+                        src={project.img}
+                        alt={project.title}
+            layout="fill"
+            className='object-cover'
+                    />
+                    </div>
+                    <div className='relative p-4 bg-[#03001417] backdrop-blur-md rounded-b-lg h-fill'>
+                        <h2 className='text-sm font-semibold text-white text-center'>
+                            {project.title}
+                        </h2>
+                    </div>
+                </Link>
+            ))}
         </div>
     </div>
   )
