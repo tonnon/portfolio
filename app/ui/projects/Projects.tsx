983f32efd4c0422503d0409c79053ce0c0a85e43
tonnon <lucastonnon@gmail.com>
2024-06-25 10:33:49 -0300

    scrollbar

--- app/ui/projects/Projects.tsx
+++ app/ui/projects/Projects.tsx
@@ -9,12 +9,12 @@ const Projects = () => {
             {projectslist.map((project, id) => (
                 <Link href={project.url} target='__blank' key={id} className="w-[383px] rounded-lg border border-neon shadow-neon">
                       <div className="relative w-full h-0 pb-[83.54%] rounded-t-lg overflow-hidden">
-                    <Image
-                        src={project.img}
-                        alt={project.title}
-            layout="fill"
-            className='object-cover'
-                    />
+                        <Image
+                            src={project.img}
+                            alt={project.title}
+                            layout="fill"
+                            className='object-cover'
+                        />
                     </div>
                     <div className='relative p-4 bg-[#03001417] backdrop-blur-md rounded-b-lg h-fill'>
                         <h2 className='text-sm font-semibold text-white text-center'>
